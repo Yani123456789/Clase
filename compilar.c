a211a25634df5c73ac2058851d9c7463d097b0a5
Yani123456789 <yaniarrazola54@gmail.com>
2026-05-12 13:38:33 -0600

    Create compilar.c

---
+++ compilar.c
@@ -0,0 +1,22 @@
+//Escribe cuál es la salida del siguiente programa:
+
+#include <stdio.h>
+#include <stdlib.h>
+main ()
+{
+  int s, j, k, i, l;
+  s=0;
+  for(j=1; j<=5; j++)
+  {
+    printf("%d", j)
+    if(j%2==0) {s=s+j; }
+  }
+  printf(""\n%d", s);
+  i=10;
+  while(i>0)
+  i=i-1;
+  printf("\n%d", i);
+  printf("\n\n");
+  system("PAUSE");
+}
+  
